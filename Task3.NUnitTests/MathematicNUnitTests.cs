667cf4bd2e56d3bdd743c2cfdcc9275c9ea2f6ae
zloychert <19860714@mail.ru>
2016-10-28 21:25:40 +0300

    Update

--- Task3.NUnitTests/MathematicNUnitTests.cs
+++ Task3.NUnitTests/MathematicNUnitTests.cs
@@ -12,6 +12,7 @@ namespace Task3.NUnitTests
         [TestCase(125, 2, 0.01)]
         [TestCase(125, 4, 0.01)]
         [TestCase(125, 5, 0.01)]
+
         public void NutonSqrt_Test(double number, int exp, double eps)
         {
             Assert.AreEqual(Mathematic.NutonSqrt(number, exp, eps), Math.Pow(number, (1 /(double)exp)), eps);
